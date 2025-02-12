e73f0ef47ee3f7217f613767d020cd6b9b044df1
RobyLedda94 <robyledda01@yahoo.it>
2025-02-12 16:08:47 +0100

    [UPD] definizione delle variabili che si appoggiano all dom

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@
 
 // SNACK 1 - Tramite una funzione calcoliamo l'area di un rettangolo moltiplicando base per altezza
 
+// Dichiarazione delle variabili che fanno riferimento agli elementi del DOM 
+
+// recupero il bottone
+let btn_area = document.getElementById('btn-area');
+// recupero l'elemento che restituisce un input a video
+let msg_area = document.getElementById('msg-area');
+
+
