cda4f5a8b73fffda8e616767a1a1c1f5ee9f1754
RobyLedda94 <robyledda01@yahoo.it>
2025-02-14 11:17:38 +0100

    [UPD] struttura html pari-dispari

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 // console.log('prova');
 
 
+
+
 // SNACK 1 - Tramite una funzione calcoliamo l'area di un rettangolo moltiplicando base per altezza
 
 // Definizione della funzione dove vado a calcolare la moltiplicazione base * altezza
